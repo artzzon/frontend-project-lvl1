8d21cdcd42fda864f6614b49ffdac7565f425e38
Artem Kazimirov <nexus.artem@gmail.com>
2021-03-07 19:25:44 +0300

    correct config eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const welcome = () => {
-    let userName = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + userName + '!');
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+};
 
 export default welcome;
