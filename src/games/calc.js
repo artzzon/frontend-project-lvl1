a9d2d848f8f84d405fc9a416880f0e4ae896fb52
artzzon <nexus.artem@gmail.com>
2021-04-22 22:46:07 +0300

    rename func mathOperation to calcOperation

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../randomNumber.js';
 
 const description = 'What is the result of the expression?';
 
-const mathOperation = (firstNumber, mathOperation, secondNumber) => {
+const calcOperation = (firstNumber, mathOperation, secondNumber) => {
   switch (mathOperation) {
     case '+':
       return firstNumber + secondNumber;
@@ -21,7 +21,7 @@ const getQuestionAndAnswer = () => {
   const secondNumber = getRandomNumber(1, 10);
   const mathSigns = '+-*';
   const randomMathOperation = mathSigns[getRandomNumber(0, mathSigns.length - 1)];
-  const answer = mathOperation(firstNumber, randomMathOperation, secondNumber);
+  const answer = calcOperation(firstNumber, randomMathOperation, secondNumber);
   const question = `${firstNumber} ${randomMathOperation} ${secondNumber}`;
   return [question, String(answer)];
 };
