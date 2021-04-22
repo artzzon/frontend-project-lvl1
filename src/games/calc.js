53bdcd77baf47fe4478b076b58ad51249b08cee8
artzzon <nexus.artem@gmail.com>
2021-04-22 22:41:02 +0300

    make const mathSigns

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,8 +3,8 @@ import getRandomNumber from '../randomNumber.js';
 
 const description = 'What is the result of the expression?';
 
-const mathOperation = (firstNumber, mathOperations, secondNumber) => {
-  switch (mathOperations) {
+const mathOperation = (firstNumber, mathSigns, secondNumber) => {
+  switch (mathSigns) {
     case '+':
       return firstNumber + secondNumber;
     case '-':
@@ -19,8 +19,8 @@ const mathOperation = (firstNumber, mathOperations, secondNumber) => {
 const getQuestionAndAnswer = () => {
   const firstNumber = getRandomNumber(1, 10);
   const secondNumber = getRandomNumber(1, 10);
-  const mathOperations = '+-*';
-  const randomMathOperation = mathOperations[getRandomNumber(0, mathOperations.length - 1)];
+  const mathSigns = '+-*';
+  const randomMathOperation = mathSigns[getRandomNumber(0, mathSigns.length - 1)];
   const answer = mathOperation(firstNumber, randomMathOperation, secondNumber);
   const question = `${firstNumber} ${randomMathOperation} ${secondNumber}`;
   return [question, String(answer)];
